59fc2f73226b50264b235ba5eb0d70af2fb4cc26
yedio <82350743+yedio@users.noreply.github.com>
2023-01-25 16:44:35 +0900

    Add: Metamask change,add Network(BSC) (#4)

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -5,20 +5,24 @@ import {
   NoEthereumProviderError,
   UserRejectedRequestError as UserRejectedRequestErrorInjected,
 } from '@web3-react/injected-connector';
-import { injected } from '../utils/web3/web3Connector';
 import { walletAddressAtom } from '../states/atom';
+import { connectors } from '../utils/web3/web3Connector';
+import { setupNetwork } from '../utils/web3/setupNetwork';
 
 const useAuth = () => {
   const { activate, deactivate } = useWeb3React();
   const setWalletAddress = useSetRecoilState(walletAddressAtom);
-
   const login = useCallback(() => {
-    if (injected) {
-      activate(injected, async (error: Error) => {
+    if (connectors.injected) {
+      activate(connectors.injected, async (error: Error) => {
         if (error instanceof NoEthereumProviderError) {
           // 지갑이 설치되어있지 않은 경우
         } else if (error instanceof UnsupportedChainIdError) {
           // 지원하지 않는 네트워크일 경우
+          const hasSetup = await setupNetwork();
+          if (hasSetup) {
+            activate(connectors.injected);
+          }
         } else if (error instanceof UserRejectedRequestErrorInjected) {
           // 유저가 지갑 연결을 거부한 경우
         } else {
